3d7ab13fe669be990b3a14f3a93d0a474a5d442a
Lajos Ambrus <Dexter245@googlemail.com>
2019-03-31 09:45:17 +0300

    added BankAccount

---
+++ csharp/bank-account/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class BankAccount
+{
+    private bool isOpen = false;
+    private decimal balance = 0;
+    private object _lock = new object();
+
+    public void Open()
+    {
+        lock (_lock)
+        {
+            isOpen = true;
+        }
+    }
+
+    public void Close()
+    {
+        lock (_lock)
+        {
+            isOpen = false;
+        }
+    }
+
+    public decimal Balance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (isOpen)
+                    return balance;
+                else
+                    throw new InvalidOperationException();
+        }
+    }
+        set
+        {
+            lock (_lock)
+            {
+                balance = value;
+            }
+        }
+    }
+
+    public void UpdateBalance(decimal change)
+    {
+        lock (_lock)
+        {
+            Balance += change;
+        }
+}
+}
