98d4795958493f0a382547932a2a34dcd18ee2c3
Lajos Ambrus <Dexter245@googlemail.com>
2019-04-03 05:58:16 +0200

    added SumOfMultiples

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+
+    static void Main(string[] args)
+    {
+        int res = SumOfMultiples.Sum(new[] { 0 }, 1);
+        Console.WriteLine("res: " + res);
+        Console.ReadLine();
+    }
+
+    public static int Sum(IEnumerable<int> multiples, int max)
+    {
+        int sum = 0;
+        for(int i = 0; i < max; i++)
+        {
+            foreach(var m in multiples)
+            {
+                if(m != 0 && i % m == 0)
+                {
+                    sum += i;
+                    break;
+                }
+            }
+        }
+        return sum;
+    }
+}
