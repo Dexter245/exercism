eb43ed0c0dcbcfe427cf192b2e4313e41ebcc1f7
Lajos Ambrus <Dexter245@googlemail.com>
2019-04-03 10:08:51 +0200

    added Grains

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class Grains
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("total: " + Total());
+        Console.ReadLine();
+    }
+
+    public static ulong Square(int n)
+    {
+        if (n <= 0 || n > 64)
+            throw new ArgumentOutOfRangeException();
+        return (ulong) Math.Pow(2, n-1);
+    }
+
+    public static ulong Total()
+    {
+        ulong sum = 0L;
+        for (int i = 1; i <= 64; i++)
+        {
+            sum += Square(i);
+        }
+        return sum;
+    }
+}
