3d8b7123e930ee80f3167d8c61cabf0e2b1226a9
Lajos Ambrus <Dexter245@googlemail.com>
2019-04-03 05:40:13 +0200

    added ReverseString

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class ReverseString
+{
+
+    static void Main(string[] args)
+    {
+        string str1 = "asdf";
+        string str2 = Reverse(str1);
+        Console.WriteLine(str1 + ": " + str2);
+        Console.ReadLine();
+    }
+
+    public static string Reverse(string input)
+    {
+        char[] text = input.ToCharArray();
+        char[] reversed = new char[text.Length];
+        for(int i = 0; i < text.Length; i++)
+        {
+            reversed[i] = text[text.Length - i - 1];
+        }
+        return new string(reversed);
+    }
+}
