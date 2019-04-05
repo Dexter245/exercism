0abb1e01f4838f72a20a02902e7ea4cb92b95db0
Lajos Ambrus <Dexter245@googlemail.com>
2019-04-05 10:19:47 +0200

    added QueenAttack

---
+++ csharp/queen-attack/QueenAttack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        if (white.Row == black.Row || white.Column == black.Column)
+            return true;
+        int diffRow = Math.Abs(white.Row - black.Row);
+        int diffCol = Math.Abs(white.Column - black.Column);
+        if (diffRow == diffCol)
+            return true;
+        return false;
+    }
+
+    public static Queen Create(int row, int column)
+    {
+        if (row < 0 || column < 0 || row >= 8 || column >= 8)
+            throw new ArgumentOutOfRangeException();
+        return new Queen(row, column);
+    }
+}
